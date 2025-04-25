a99c7c69ee274e428e98304591a0a4a27d1fa534
Nate Royer <natroyer@Old-mans-M1.local>
2025-04-25 11:50:37 -0600

    Use localhost instead of any address for default

--- config.py
+++ config.py
@@ -1,14 +1,23 @@
+import logging
+
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     API_DOMAIN: str = "localhost"
-    API_IPADDRESS: str = "0.0.0.0"
+    API_IPADDRESS: str = "127.0.0.1"
     API_PORT: int = 8000
     LOG_LEVEL: str = "WARNING"
     RELOAD: bool = False
 
-    class ConfigDict:
+    @property
+    def log_level(self):
+        return logging.getLevelName(self.LOG_LEVEL.upper())
+
+    # class ConfigDict: <--- not picking up LOG_LEVEL, why?
+    # https://docs.pydantic.dev/2.11/api/config/#pydantic.config.ConfigDict
+    # https://docs.pydantic.dev/2.11/migration/#changes-to-config
+    class Config:
         env_file = ".env"
         # env_prefix = "MYAPP_"  # Use a prefix to avoid namespace collisions
 
